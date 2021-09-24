c15b878c1ae34f78031c71b276678fa5f2f41994
N59482 <iduginmail@ya.ru>
2021-09-24 15:02:28 +0300

    Add ReverseInt.cpp

--- ReverseInt.cpp
+++ ReverseInt.cpp
@@ -3,26 +3,24 @@ public:
     int reverse(int x) 
         {
             int res = 0;
-            if (x != 0)
-                {
-                    std::vector<int> tempV;
-                    int k = 0; // коэффициэнт 
-                    bool negative = 0;
-                    if(x<0) { negative = 1; x = x * (-1);}
-                    while (x != 0)
+            int pre_max_int = 214748364;
+            bool negative = 0;
+            if(x < 0) { negative = 1; x = x * (-1);}
+            /*
+сложное условие while - обьясняю:
+            while (усл1 && (усл2 && усл3)) где: 
+усл 1. (x != 0) - входное число не закончилось
+усл 2. (res <= pre_max_int) - промежуточный результат не больше чем MAX_INT % 10
+усл 3. (x % 10) <= (7 + int(negative)) - (x % 10) не больше чем последняя цифра макс\мин значения int (7 или 8 в зависимости от знака) диапазон значений int - 2...8 < x < 2..7
+            */
+                    while ((x != 0) && ((res <= pre_max_int) && ((x % 10) <= (7 + int(negative)))))
                         {
-                            tempV.push_back(x % 10);
+                            res *= 10;
+                            res += x % 10;
                             x = x / 10;
                         };
-                    k = pow(10, tempV.size()-1); // коэффициэнт 
-                    for (int a : tempV) // 
-                        {
-                            res += a * k; //
-                            k /= 10; // коэфициэнт делится на 10.
-                        };
                     if (negative) res = res * (-1);
-
-                };
+                    if (x != 0) res = 0;
             return res;
         };
 };
