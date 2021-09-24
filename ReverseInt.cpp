ff9676069a6791b232f5e8f0be4a90e018c18c1f
N59482 <iduginmail@ya.ru>
2021-09-24 15:24:14 +0300

    Add ReverseInt.cpp

--- ReverseInt.cpp
+++ ReverseInt.cpp
@@ -4,12 +4,13 @@ public:
         {
             int res = 0;
             int pre_max_int = 214748364;
+            int pre_mix_int = -214748364;
             bool negative = 0;
-            if(x < 0) { negative = 1; x = x * (-1);}
                     while (x != 0)
                         {
-                            if (res > pre_max_int) return 0;
-                            else if ((res == pre_max_int) && ((x % 10) > (7 + int(negative)))) return 0;
+                            if ((res > pre_max_int) || (res < pre_mix_int)) return 0;
+                            else if ((res == pre_max_int) && ((x % 10) > 7 )) return 0;
+                                else if ((res == pre_mix_int) && ((x % 10) > 8 )) return 0;
                             res *= 10;
                             res += x % 10;
                             x = x / 10;
